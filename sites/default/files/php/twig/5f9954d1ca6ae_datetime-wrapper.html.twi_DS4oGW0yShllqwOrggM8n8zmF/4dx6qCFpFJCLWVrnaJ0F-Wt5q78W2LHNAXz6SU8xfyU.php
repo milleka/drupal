96f364e55cabc98bf4f5cee6638e2db45919015b
Chassaing Camille <camillechassaing.jdb@gmail.com>
2020-10-28 19:02:21 +0100

    create page competences

---
+++ sites/default/files/php/twig/5f9954d1ca6ae_datetime-wrapper.html.twi_DS4oGW0yShllqwOrggM8n8zmF/4dx6qCFpFJCLWVrnaJ0F-Wt5q78W2LHNAXz6SU8xfyU.php
@@ -0,0 +1,125 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/modules/system/templates/datetime-wrapper.html.twig */
+class __TwigTemplate_f4d5894ddec26b66662ede898268a51cd4b0070ab6d46c225780cdf050f3c6eb extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["set" => 19, "if" => 24];
+        $filters = ["escape" => 25];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 19
+        $context["title_classes"] = [0 => ((        // line 20
+($context["required"] ?? null)) ? ("js-form-required") : ("")), 1 => ((        // line 21
+($context["required"] ?? null)) ? ("form-required") : (""))];
+        // line 24
+        if (($context["title"] ?? null)) {
+            // line 25
+            echo "  <h4";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["title_attributes"] ?? null), "addClass", [0 => ($context["title_classes"] ?? null)], "method")), "html", null, true);
+            echo ">";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null)), "html", null, true);
+            echo "</h4>
+";
+        }
+        // line 27
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null)), "html", null, true);
+        echo "
+";
+        // line 28
+        if (($context["errors"] ?? null)) {
+            // line 29
+            echo "  <div>
+    ";
+            // line 30
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["errors"] ?? null)), "html", null, true);
+            echo "
+  </div>
+";
+        }
+        // line 33
+        if (($context["description"] ?? null)) {
+            // line 34
+            echo "  <div";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["description_attributes"] ?? null)), "html", null, true);
+            echo ">
+    ";
+            // line 35
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["description"] ?? null)), "html", null, true);
+            echo "
+  </div>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "core/modules/system/templates/datetime-wrapper.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  91 => 35,  86 => 34,  84 => 33,  78 => 30,  75 => 29,  73 => 28,  69 => 27,  61 => 25,  59 => 24,  57 => 21,  56 => 20,  55 => 19,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/modules/system/templates/datetime-wrapper.html.twig", "C:\\wamp64\\www\\drupal\\core\\modules\\system\\templates\\datetime-wrapper.html.twig");
+    }
+}
---
+++ sites/default/files/php/twig/5f9954d1ca6ae_fontawesomeicon.html.twig_Nf8cnpl79t_UgO_gXY2wyoSkW/nZRk9ELnNpm-2TEXaHELemx_wzfQCl0IVhVo3dB1fy4.php
@@ -0,0 +1,105 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/fontawesome/templates/fontawesomeicon.html.twig */
+class __TwigTemplate_f0f6bfaa75914b79324353369005e18ec100d5877cabe26db78562cd3ca35cee extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = [];
+        $filters = ["escape" => 19];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 18
+        echo "<div class=\"fontawesome-icon\">
+  <";
+        // line 19
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["tag"] ?? null)), "html", null, true);
+        echo " class=\"";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["style"] ?? null)), "html", null, true);
+        echo " ";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["name"] ?? null)), "html", null, true);
+        echo " ";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["settings"] ?? null)), "html", null, true);
+        echo "\" data-fa-transform=\"";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["transforms"] ?? null)), "html", null, true);
+        echo "\" data-fa-mask=\"";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["mask"] ?? null)), "html", null, true);
+        echo "\" style=\"";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["css"] ?? null)), "html", null, true);
+        echo "\"></";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["tag"] ?? null)), "html", null, true);
+        echo ">
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/fontawesome/templates/fontawesomeicon.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  58 => 19,  55 => 18,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/fontawesome/templates/fontawesomeicon.html.twig", "C:\\wamp64\\www\\drupal\\modules\\fontawesome\\templates\\fontawesomeicon.html.twig");
+    }
+}
